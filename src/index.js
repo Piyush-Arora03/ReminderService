d8e1929fdfbeb914c6a3e342e7ccedcc6c202c56
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2025-07-27 14:44:20 +0530

    setup reminder via booking

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const body_parser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const setUpJob = require('./utils/job');
 const apiRoutes = require('./routes/index');
-const {subscribeToQueue} = require('./utils/MessageQueue');
+const { subscribeToQueue } = require('./utils/MessageQueue');
 const EmailService = require('./services/emailService');
 const emailService = new EmailService();
 
@@ -16,8 +16,18 @@ const SetUpAndStartServer = () => {
     subscribeToQueue(async (message) => {
         // console.log('Received message from queue:', message);
         await emailService.sendConfirmationEmail(message);
+        const flightTime = newDate(message.departureTime);
+        const notificationTime = newDate(flightTime.getTime() - 24 * 60 * 60 * 1000);
+        const notificationPayload = {
+            subject: `Reminder: Your flight is in 24 hours`,
+            content: `This is a reminder for your flight scheduled at ${flightTime.toLocaleString()}.`,
+            recepientEmail: data.recepientEmail,
+            notificationTime: notificationTime,
+            status: 'PENDING'
+        };
+        await emailService.createTicket(notificationPayload);
         // console.log('Confirmation email sent for message:', message);
-    }, 'NOTIFICATION_SERVICE'); 
+    }, 'NOTIFICATION_SERVICE');
     app.listen(PORT, () => {
         console.log(`server started at ${PORT}`);
         // setUpJob();
